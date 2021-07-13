8b416243a365d56596ba69cd55481a53b38af504
smk <44811918+mightyjoe781@users.noreply.github.com>
2021-07-13 22:56:51 +0530

    Cleanup

---
+++ cpp/demux/test.cpp
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------------
+// Standard Libraries
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//--------------------------------------------------------------------
+// Main functions
+
+int main(){
+
+    ios_base::sync_with_stdio(false);   //Fast input and output
+    cin.tie(NULL);                      //Flush output
+    #ifndef ONLINE_JUDGE                //Online judge config
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+
